104b7fc4cca842c67ec9fd8b0b4513e318704d13
Lon Kastenson <lhkastenson@gmail.com>
2014-04-27 22:50:55 -0500

    3.5 Exercise 2 string interpolation ex

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Hom page" do
 
     it "should have the content 'Sample App" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the title 'Hom'" do
       visit '/static_pages/hom'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Hom")
+      expect(page).to have_title("#{base_title} | Hom")
     end
   end
 
@@ -23,7 +25,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -35,7 +37,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -47,7 +49,7 @@ describe "StaticPages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
   
