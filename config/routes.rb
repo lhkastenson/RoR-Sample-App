e5cc5da8ba0080785a21217ab95fb74561929d61
Lon Kastenson <lhkastenson@gmail.com>
2014-05-05 23:41:01 -0500

    signin path fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'users#new',            via: 'get'
   #  get "static_pages/hom"
   #  get "static_pages/help"
   #  get "static_pages/about"
